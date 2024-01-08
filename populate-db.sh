97c95b6629733683aaf256ad9f0778f5928f8630
Alexander Petros <apetros15@gmail.com>
2024-01-07 22:36:24 -0500

    Filter blank metadata and don't limit to MP3s

--- populate-db.sh
+++ populate-db.sh
@@ -15,9 +15,10 @@ for dir in ./ipods/*/; do
   ipod_id=$(sqlite3 -init /dev/null "$DB_NAME" "$ipod_insert_query" 2>/dev/null)
 
   echo "Archiving the tracks on $ipod_name"
-  find "$dir" -name '*.mp3' -type f -print0 |
+  find "$dir" -type f -print0 |
     xargs -0 mediainfo --Inform="General;%Title%~%Album%~%Artist%\n" |
     sql_escape |
+    grep -v '^~~$' |
     while IFS="~" read -r title album artist; do
       sqlite3 -init /dev/null "$DB_NAME" <<EOF
       INSERT INTO songs(ipod_id, title, album, artist)
