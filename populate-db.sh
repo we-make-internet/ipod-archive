2b1d7ae40e02f4a715d5e281e6bbf8dabc7dc10a
Alexander Petros <apetros15@gmail.com>
2024-01-07 20:21:48 -0500

    Slight bash refactor

--- populate-db.sh
+++ populate-db.sh
@@ -4,22 +4,24 @@ set -euo pipefail
 
 DB_NAME='archive.db'
 
-# Use the find command to locate all files in the subdirectories
-# The mv command then moves each file to the root directory
+function sql_escape {
+  sed "s/'/''/g"
+}
 
 for dir in ./ipods/*/; do
   # Get the name of the folder, and escape any single quotes in it
-  ipod_name=$(basename "$dir" | sed "s/'/''/g")
-  ipod_id=$(sqlite3 -init /dev/null "$DB_NAME" "INSERT INTO ipods(name) VALUES('$ipod_name') RETURNING ipod_id" 2>/dev/null)
+  ipod_name=$(basename "$dir" | sql_escape)
+  ipod_insert_query="INSERT INTO ipods(name) VALUES('$ipod_name') RETURNING ipod_id;"
+  ipod_id=$(sqlite3 -init /dev/null "$DB_NAME" "$ipod_insert_query" 2>/dev/null)
 
   echo "Archiving the tracks on $ipod_name"
   find "$dir" -name '*.mp3' -type f -print0 |
     xargs -0 mediainfo --Inform="General;%Title%~%Album%~%Artist%\n" |
     while IFS= read -r line; do
       IFS="~" read -r title album artist
-      title=$(echo "$title" |  sed "s/'/''/g")
-      album=$(echo "$album" |  sed "s/'/''/g")
-      artist=$(echo "$artist" |  sed "s/'/''/g")
+      title=$(echo "$title" | sql_escape)
+      album=$(echo "$album" | sql_escape)
+      artist=$(echo "$artist" | sql_escape)
       sqlite3 -init /dev/null "$DB_NAME" 2>/dev/null <<EOF
       INSERT INTO songs(ipod_id, title, album, artist)
       VALUES ($ipod_id, '$title', '$album', '$artist');
